1af93ef4626c23ca0a6fec125b1001b5e77d2a4a
xlc-dev <119954023+xlc-dev@users.noreply.github.com>
2024-11-24 20:37:12 +0100

    fix type error

--- src/twinworlds.svelte.ts
+++ src/twinworlds.svelte.ts
@@ -9,11 +9,11 @@ The frequency of use and power usage are randomized for each appliance.
 const createTwinWorld = (
   baseHouseholdCount: number,
   variation: number,
-  description: string
+  description: string,
 ): TwinWorld => {
   const householdCount = getRandomInt(
     Math.max(1, baseHouseholdCount - variation),
-    baseHouseholdCount + variation
+    baseHouseholdCount + variation,
   );
 
   return {
@@ -50,12 +50,12 @@ export const defaultTwinWorlds: { [key: string]: TwinWorld } = {
   "Twin World small": createTwinWorld(
     25,
     5,
-    `A small twin world consisting of roughly 25 households. ${TWIN_WORLD_DESCRIPTION}`
+    `A small twin world consisting of roughly 25 households. ${TWIN_WORLD_DESCRIPTION}`,
   ),
   "Twin World large": createTwinWorld(
     75,
     5,
-    `A large twin world consisting of roughly 75 households. ${TWIN_WORLD_DESCRIPTION}`
+    `A large twin world consisting of roughly 75 households. ${TWIN_WORLD_DESCRIPTION}`,
   ),
 };
 
@@ -133,10 +133,12 @@ const steps: Step[] = $state([
     title: "Twin World",
     options: [
       {
+        id: "1",
         label: "Twin World small",
         description: `A small twin world consisting of roughly 25 households. ${TWIN_WORLD_DESCRIPTION}`,
       },
       {
+        id: "2",
         label: "Twin World large",
         description: `A large twin world consisting of roughly 75 households. ${TWIN_WORLD_DESCRIPTION}`,
       },
@@ -160,7 +162,8 @@ const steps: Step[] = $state([
       },
       {
         label: "Solar Panel Capacity",
-        description: "Enter the average yield of a single solar panel in a year",
+        description:
+          "Enter the average yield of a single solar panel in a year",
         type: "input",
         dataType: "int",
         placeholder: "340",
@@ -173,11 +176,13 @@ const steps: Step[] = $state([
     title: "Cost Model",
     options: [
       {
+        id: "1",
         label: "Fixed Price",
         description:
           "A fixed price for buying and selling energy. The price for buying from the utility is 0.4 and the price for selling is 0.1. The price is determined by 0.25. A higher fixed devisision means a higher trading price.",
       },
       {
+        id: "2",
         label: "TEMO",
         description:
           "A price model based on the TEMO model. The price is determined by a formula that compares the energy needed to the various prices available, and returns an internal buying and selling prices",
@@ -232,7 +237,8 @@ const steps: Step[] = $state([
       },
       {
         label: "Cost Model Algorithm",
-        description: "A custom formula used to determine the internal buying and selling price",
+        description:
+          "A custom formula used to determine the internal buying and selling price",
         type: "editor",
         required: true,
         value: `function costModel() {\n\treturn buyCustomer * ratio + sellCustomer * (1 - ratio);\n}\n`,
@@ -243,11 +249,13 @@ const steps: Step[] = $state([
     title: "Algorithm",
     options: [
       {
+        id: "1",
         label: "Greedy Planning",
         description:
           "An initial planning that puts appliances in their local optimum through a greedy algorithm. Will not optimize further than one pass through all appliances.",
       },
       {
+        id: "2",
         label: "Simulated Annealing",
         description:
           "An algorithm that improves on a given algorithm by randomly changing the time of planned in appliances. The conditions for what changes becomes stricter over time, resulting in a further optimized solution.",
@@ -279,7 +287,8 @@ const steps: Step[] = $state([
       },
       {
         label: "Custom Algorithm",
-        description: "A custom algorithm used to determine when an appliance will be planned in",
+        description:
+          "A custom algorithm used to determine when an appliance will be planned in",
         type: "editor",
         required: true,
         value: `function run() {\n\treturn true;\n}\n`,
@@ -290,6 +299,7 @@ const steps: Step[] = $state([
     title: "Energyflow",
     options: [
       {
+        id: "1",
         label: "Energyflow Zoetermeer",
         description:
           "The energy data from a green household in Zoetermeer that is associated with the THUAS.",
@@ -313,7 +323,8 @@ const steps: Step[] = $state([
       },
       {
         label: "Solar Panels Factor",
-        description: "The amount of solar panels for the household in the supplied energy table",
+        description:
+          "The amount of solar panels for the household in the supplied energy table",
         type: "input",
         dataType: "int",
         placeholder: "25",
