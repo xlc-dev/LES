2ce0f4ef51ede703b21b1a93ec80343107ed9def
xlc-dev <119954023+xlc-dev@users.noreply.github.com>
2024-12-08 22:13:56 +0100

    cleanup old stuff

--- src/Simulation.svelte
+++ src/Simulation.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-  import Sidebar from "./Sidebar.svelte";
-  import HouseholdView from "./HouseholdView.svelte";
   import HouseSVG from "./HouseSVG.svelte";
   import Filter from "./Filter.svelte";
 
-  import { getHousehold, getStepperData } from "./state.svelte";
+  import { getStepperData } from "./state.svelte";
 
   const stepperData = getStepperData();
-  const household = getHousehold();
 
   let searchQuery: string = $state("");
 
@@ -45,21 +42,12 @@
   });
 </script>
 
-<div class="flex flex-col md:flex-row h-screen">
-  <Sidebar />
-  <div class="flex-1 overflow-auto p-4 text-les-highlight md:ml-64">
-    <div class="flex flex-col gap-8">
-      {#if household.household}
-        <HouseholdView household={household.household} />
-      {:else}
-        <Filter {filters} bind:selectedFilters bind:searchQuery />
-        <div
-          class="grid grid-cols-8 items-center justify-between gap-4 rounded-lg border-4 border-gray-400 p-2 bg-white">
-          {#each filteredHouseholds as data}
-            <HouseSVG {household} {data} />
-          {/each}
-        </div>
-      {/if}
-    </div>
+<div class="flex flex-col gap-8">
+  <Filter {filters} bind:selectedFilters bind:searchQuery />
+  <div
+    class="grid grid-cols-8 items-center justify-start gap-4 rounded-lg border-4 border-gray-400 p-2 bg-white">
+    {#each filteredHouseholds as data}
+      <HouseSVG {data} />
+    {/each}
   </div>
 </div>
