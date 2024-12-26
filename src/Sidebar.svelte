17b77aa403559bf9180092b9b804030d1ac5eeee
xlc-dev <119954023+xlc-dev@users.noreply.github.com>
2024-12-26 14:33:34 +0100

    fix prettier brainfart

--- src/Sidebar.svelte
+++ src/Sidebar.svelte
@@ -29,18 +29,19 @@
 
 <!-- Mobile Sidebar -->
 <div
-  class="fixed left-0 top-0 h-full w-64 bg-gray-900 z-50
-          transition-transform duration-300 ease-in-out
-          lg:hidden sidebar-container
+  class="sidebar-container fixed top-0 left-0 z-50 h-full w-64
+          bg-gray-900 transition-transform duration-300
+          ease-in-out lg:hidden
           {isMenuOpen ? 'translate-x-0' : '-translate-x-full'} flex flex-col">
   <div class="grow overflow-auto">
-    <div class="flex w-full items-center gap-3 bg-les-gray-700 p-4">
+    <div class="bg-les-gray-700 flex w-full items-center gap-3 p-4">
       <img src="/arrows.png" alt="" class="rotate-180 transform" />
       <p class="font-bold">LES</p>
     </div>
     <div class="mt-4 flex flex-col items-start justify-center gap-3">
       <button
-        class={`flex w-full items-center gap-3 p-4 transition-colors duration-200 hover:bg-les-gray-700 ${component.currentComponent === "Dashboard" ? "bg-les-gray-700" : ""}`}
+        class="hover:bg-les-gray-700 flex w-full items-center gap-3 p-4 transition-colors duration-200"
+        class:bg-les-gray-700={component.currentComponent === "Dashboard"}
         onclick={() => {
           component.setComponent("Dashboard");
           isMenuOpen = false;
@@ -49,7 +50,8 @@
         <p>Dashboard</p>
       </button>
       <button
-        class={`flex w-full items-center gap-3 p-4 transition-colors duration-200 hover:bg-les-gray-700 ${component.currentComponent === "Simulation" ? "bg-les-gray-700" : ""}`}
+        class="hover:bg-les-gray-700 flex w-full items-center gap-3 p-4 transition-colors duration-200"
+        class:bg-les-gray-700={component.currentComponent === "Simulation"}
         onclick={() => {
           component.setComponent("Simulation");
           isMenuOpen = false;
@@ -58,7 +60,8 @@
         <p>Simulation</p>
       </button>
       <button
-        class={`flex w-full items-center gap-3 p-4 transition-colors duration-200 hover:bg-les-gray-700 ${component.currentComponent === "Schedulable Loads" ? "bg-les-gray-700" : ""}`}
+        class="hover:bg-les-gray-700 flex w-full items-center gap-3 p-4 transition-colors duration-200"
+        class:bg-les-gray-700={component.currentComponent === "Schedulable Loads"}
         onclick={() => {
           component.setComponent("Schedulable Loads");
           isMenuOpen = false;
@@ -70,7 +73,7 @@
   </div>
   <div class="shrink-0">
     <button
-      class="flex w-full items-center gap-3 p-4 transition-colors duration-200 bg-les-highlight hover:bg-les-gray-700 text-white"
+      class="bg-les-highlight hover:bg-les-gray-700 flex w-full items-center gap-3 p-4 text-white transition-colors duration-200"
       aria-label="Show Legend"
       onclick={() => {
         showLegend = !showLegend;
@@ -105,51 +108,42 @@
 
 <!-- Top Bar for Desktop -->
 <div
-  class="hidden lg:flex w-full bg-gray-900 px-4 py-2 items-center justify-between border-b border-gray-800 sticky">
+  class="sticky hidden w-full items-center justify-between border-b border-gray-800 bg-gray-900 px-4 py-2 lg:flex">
   <div class="flex items-center gap-2">
-    <img src="/arrows.png" alt="" class="rotate-180 transform h-6 w-6" />
+    <img src="/arrows.png" alt="" class="h-6 w-6 rotate-180 transform" />
     <span class="text-lg font-bold text-white">LES</span>
   </div>
   <nav class="flex items-center space-x-4">
     <button
-      class={`flex items-center gap-1.5 px-2 py-1.5 rounded-md transition-colors duration-200 text-sm cursor-pointer
-        ${
-          component.currentComponent === "Dashboard"
-            ? "bg-les-highlight text-white"
-            : "text-gray-300 hover:bg-les-gray-700 hover:text-white"
-        }`}
+      class="hover:bg-les-gray-700 flex cursor-pointer items-center gap-1.5 rounded-md px-2 py-1.5 text-sm text-gray-300 transition-colors duration-200 hover:text-white"
+      class:bg-les-highlight={component.currentComponent === "Dashboard"}
+      class:text-white={component.currentComponent === "Dashboard"}
       onclick={() => component.setComponent("Dashboard")}>
       <img src="/rectangle.png" alt="" class="h-4 w-4" />
       <span>Dashboard</span>
     </button>
     <button
-      class={`flex items-center gap-1.5 px-2 py-1.5 rounded-md transition-colors duration-200 text-sm cursor-pointer
-        ${
-          component.currentComponent === "Simulation"
-            ? "bg-les-highlight text-white"
-            : "text-gray-300 hover:bg-les-gray-700 hover:text-white"
-        }`}
+      class="hover:bg-les-gray-700 flex cursor-pointer items-center gap-1.5 rounded-md px-2 py-1.5 text-sm text-gray-300 transition-colors duration-200 hover:text-white"
+      class:bg-les-highlight={component.currentComponent === "Simulation"}
+      class:text-white={component.currentComponent === "Simulation"}
       onclick={() => component.setComponent("Simulation")}>
       <img src="/adjustment.png" alt="" class="h-4 w-4" />
       <span>Simulation</span>
     </button>
     <button
-      class={`flex items-center gap-1.5 px-2 py-1.5 rounded-md transition-colors duration-200 text-sm cursor-pointer
-        ${
-          component.currentComponent === "Schedulable Loads"
-            ? "bg-les-highlight text-white"
-            : "text-gray-300 hover:bg-les-gray-700 hover:text-white"
-        }`}
+      class="hover:bg-les-gray-700 flex cursor-pointer items-center gap-1.5 rounded-md px-2 py-1.5 text-sm text-gray-300 transition-colors duration-200 hover:text-white"
+      class:bg-les-highlight={component.currentComponent === "Schedulable Loads"}
+      class:text-white={component.currentComponent === "Schedulable Loads"}
       onclick={() => component.setComponent("Schedulable Loads")}>
       <img src="/calendar.png" alt="" class="h-4 w-4" />
       <span>Schedulable Loads</span>
     </button>
   </nav>
   <div class="flex items-center space-x-3">
-    <div class="text-right mr-2">
+    <div class="mr-2 text-right">
       <div class="flex flex-col space-y-0.5">
-        <div class="text-2xs text-gray-400 uppercase tracking-wider">Selected Configuration</div>
-        <div class="text-xs text-gray-200 leading-tight">
+        <div class="text-2xs tracking-wider text-gray-400 uppercase">Selected Configuration</div>
+        <div class="text-xs leading-tight text-gray-200">
           {stepperData.stepperData.twinworld.name} ·
           {stepperData.stepperData.costmodel.name} ·
           {stepperData.stepperData.algo.name} ·
@@ -158,15 +152,15 @@
       </div>
     </div>
     <button
-      class="flex items-center gap-1.5 text-white px-3 py-1.5 rounded-md text-sm
-             transition-colors duration-200 cursor-pointer bg-les-highlight hover:bg-les-gray-700"
+      class="bg-les-highlight hover:bg-les-gray-700 flex cursor-pointer items-center gap-1.5 rounded-md px-3
+             py-1.5 text-sm text-white transition-colors duration-200"
       onclick={() => (showLegend = !showLegend)}>
       <span>Show Legend</span>
     </button>
     <button
       id="stop-button"
-      class="flex items-center gap-1.5 bg-red-950 text-white px-3 py-1.5 rounded-md text-sm
-             hover:bg-red-900 transition-colors duration-200 cursor-pointer"
+      class="flex cursor-pointer items-center gap-1.5 rounded-md bg-red-950 px-3 py-1.5 text-sm
+             text-white transition-colors duration-200 hover:bg-red-900"
       onclick={stop}>
       <img src="/stop.png" alt="" class="h-4 w-4" />
       <span>Stop</span>
@@ -176,7 +170,7 @@
 
 <!-- Mobile Hamburger Button -->
 <button
-  class="fixed top-4 left-4 z-50 lg:hidden bg-les-highlight p-2 rounded-sm hamburger-button"
+  class="bg-les-highlight hamburger-button fixed top-4 left-4 z-50 rounded-sm p-2 lg:hidden"
   class:hidden={isMenuOpen}
   aria-label="Open menu"
   onclick={() => (isMenuOpen = !isMenuOpen)}>
@@ -195,14 +189,14 @@
 </button>
 
 {#if isMenuOpen}
-  <div class="fixed inset-0 bg-black/90 bg-opacity-50 z-40 lg:hidden"></div>
+  <div class="bg-opacity-50 fixed inset-0 z-40 bg-black/90 lg:hidden"></div>
 {/if}
 
 {#if showLegend}
   <div
-    class="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-white p-4 rounded-lg shadow-lg border-4 border-gray-400 text-les-highlight legend-container w-3/4 lg:w-full max-w-2xl mx-auto">
+    class="text-les-highlight legend-container absolute top-1/3 left-1/2 z-50 mx-auto w-3/4 max-w-2xl -translate-x-1/2 -translate-y-1/2 transform rounded-lg border-4 border-gray-400 bg-white p-4 shadow-lg lg:w-full">
     <button
-      class="cursor-pointer absolute top-0 right-0 p-2 text-xs text-les-highlight hover:text-blue-500 transition-colors duration-200"
+      class="text-les-highlight absolute top-0 right-0 cursor-pointer p-2 text-xs transition-colors duration-200 hover:text-blue-500"
       aria-label="Close legend"
       onclick={() => (showLegend = !showLegend)}>
       <svg
